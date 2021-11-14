60fb4e8a19d57069656067b5951bd63d7f5a13e1
Cuong Dong Minh <mcd.oblivion@gmail.com>
2021-11-14 15:12:02 +0700

    Add config for webapp

---
+++ webapp.php
@@ -0,0 +1,12 @@
+<?php
+define("TOKEN", "secret-token");                                                    // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
+define("REMOTE_REPOSITORY", "https://gitlab.com/accommerce/webapp.git");      // The SSH URL to your repository
+define("DIR", "/home/ubuntu/accommerce/webapp");                             // The path to your repostiroy; this must begin with a forward slash (/)
+define("BRANCH", "refs/heads/master");                                              // The branch route
+define("LOGFILE", "deploy.log");                                                    // The name of the file you want to log to.
+define("GIT", "/usr/bin/git");                                                      // The path to the git executable
+define("MAX_EXECUTION_TIME", 180);                                                  // Override for PHP's max_execution_time (may need set in php.ini)
+define("BEFORE_PULL", "/usr/bin/git reset --hard @{u}");
+define("AFTER_PULL", "/usr/bin/yarn & /usr/bin/yarn build");         // A command to execute after successfully pulling
+
+require_once("deployer.php");
